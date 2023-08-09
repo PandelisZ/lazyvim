35d1fa00941653169169b08b553a8aa93298d2d1
Pandelis Zembashis <p@ndel.is>
2023-08-09 14:16:55 +0100

    feat: config

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,53 @@
+return {
+  { "nvim-lua/plenary.nvim" },
+  {
+    "ThePrimeagen/harpoon",
+    cmd = "Harpoon",
+    keys = {
+      {
+        "<leader>hm",
+        function()
+          require("harpoon.mark").add_file()
+        end,
+        desc = "Add file to harpoon",
+      },
+      {
+        "<leader>hh",
+        function()
+          require("harpoon.ui").toggle_quick_menu()
+        end,
+        desc = "Toggle harpoon menu",
+      },
+      {
+        "<leader>hj",
+        function()
+          require("harpoon.ui").nav_next()
+        end,
+      },
+      {
+        "<leader>hk",
+        function()
+          require("harpoon.ui").nav_prev()
+        end,
+      },
+      {
+        "<leader>h1",
+        function()
+          require("harpoon.ui").nav_file(1)
+        end,
+      },
+      {
+        "<leader>h2",
+        function()
+          require("harpoon.ui").nav_file(2)
+        end,
+      },
+      {
+        "<leader>h3",
+        function()
+          require("harpoon.ui").nav_file(3)
+        end,
+      },
+    },
+  },
+}
