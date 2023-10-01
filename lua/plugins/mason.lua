73e735bda9d1c25728b14c0adb5b7015dcb261a4
Pandelis Zembashis <p@ndel.is>
2023-10-01 22:25:46 +0100

    nvim

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,18 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "actionlint",
+      "blade-formatter",
+      "eslint_d",
+      "html-lsp",
+      "intelephense",
+      "php-debug-adapter",
+      "phpstan",
+      "pint",
+      "prettierd",
+      "stylua",
+      "tailwindcss-language-server",
+    },
+  },
+}
