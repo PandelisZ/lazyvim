b71bd63e26c1ffae0c5edd816bcbb604d896c059
Pandelis Zembashis <p@ndel.is>
2024-09-06 14:55:14 +0100

    chore: pretteir actually works

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,26 +1,19 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    init = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      -- add a keymap
-      keys[#keys + 1] = { "<leader>ts", ":LspRestart<CR>", "Restart LSP" }
-    end,
     opts = {
-      servers = {
-        --astro = {},
-        --tailwindcss = {},
-        eslint = {},
+      servers = { eslint = {} },
+      setup = {
+        eslint = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
       },
-      eslint = function()
-        require("lazyvim.util").lsp.on_attach(function(client)
-          if client.name == "eslint" then
-            client.server_capabilities.documentFormattingProvider = true
-          elseif client.name == "tsserver" then
-            client.server_capabilities.documentFormattingProvider = false
-          end
-        end)
-      end,
     },
   },
 }
